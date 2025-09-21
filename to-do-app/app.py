678244008ae353705d6cae787bab763e6b30abd9
sophiarequiao <162449038+sophiarequiao@users.noreply.github.com>
2025-09-21 21:13:04 +0200

    TO DO APP

---
+++ to-do-app/app.py
@@ -0,0 +1,64 @@
+import database
+from datetime import date
+import datetime
+from tabulate import tabulate
+from colorama import Fore, Style
+
+MENU_PROMPT=""" -- To Do App --
+
+Please choose one of these option:
+
+1) Add a new task.
+2) See tasks for a specific day.
+3) Check task.
+4) Exit.
+
+Your selection: """
+
+def menu():
+    connection = database.create_connection()
+
+    database.create_table(connection)
+
+    while (userOption := input(MENU_PROMPT))!="4":
+        match userOption:
+            case "1":
+                task_name = input("Name: ")
+                task_priority = input("Priority: ")
+                task_date = toDate(input("Date(dd/mm/yyyy): "))
+                database.add_job(connection, task_name,task_date, task_priority)
+            case "2":
+                user_date = toDate(input("Date(dd/mm/yyyy): "))
+                listTasks = database.see_job_specific_day(connection, user_date)
+                formatted = []
+                for task in listTasks:
+                    idFromTask, name, date, priority, done = task
+                    if done=='S': 
+                        name = f"\033[9m{name}\033[0m"  # <-- tachado
+                    formatted.append((name, priority))
+                print(tabulate(formatted,headers = [ "NAME", "PRIORITY"],tablefmt="rst"))
+            case "3":
+                task_name = input("Name: ")
+                task_date = toDate(input("Date(dd/mm/yyyy): "))
+                database.check_done(connection, task_name, task_date)
+            case _:
+                print("Invalid option")
+
+        
+    database.close_connection(connection)
+
+def toDate(dateOfUser):
+    while True:
+        try:
+            day,month,year = dateOfUser.split("/")
+            dateOfUser = datetime.date(int(year),int(month),int(day))
+            if dateOfUser < date.today():
+                dateOfUser = input(f"{Fore.RED}WARNING:{Style.RESET_ALL} Please do not write a date in the past!!\n" \
+                         "Date: ")
+            else:
+                return dateOfUser
+        except (ValueError, IndexError):
+            dateOfUser = input(f"{Fore.RED}WARNING:{Style.RESET_ALL} Please write the date as dd/mm/yyyy!!\n" \
+                                "Date: ")
+
+menu()
--- to-do-app/database.py
+++ to-do-app/database.py
@@ -1,14 +1,17 @@
 import sqlite3
 
-CREATE_TODO_TABLE = """CREATE TABLE IF NOT EXIST todo(
+CREATE_TODO_TABLE = """CREATE TABLE IF NOT EXISTS todo(
                         id          INTEGER PRIMARY KEY,
                         name        TEXT NOT NULL,
                         date        DATE NOT NULL,
-                        priority    INTEGER
+                        priority    INTEGER,
                         done        varchar(1) DEFAULT 'N')"""
 
 ADD_JOB = """INSERT INTO todo(name, date, priority) VALUES (?,?,?);"""
 
+UPDATE_JOB = """UPDATE todo SET done = 'S' WHERE name = ? AND date = ?;"""
+
+SEE_JOB_SPECIFIC_DAY = """SELECT * FROM todo WHERE date = ? ORDER BY priority ASC"""
 
 def create_connection():
     return sqlite3.connect("data.db")
@@ -20,4 +23,14 @@ def create_table(connection):
 def add_job(connection, name, date, priority):
     with connection:
         connection.execute(ADD_JOB, (name, date, priority))
+
+def check_done(connection,name,date):
+    with connection:
+        connection.execute(UPDATE_JOB, (name, date))
+
+def see_job_specific_day(connection,date):
+    with connection:
+        return connection.execute(SEE_JOB_SPECIFIC_DAY, (date,)).fetchall()
     
+def close_connection(connection):
+    connection.close() #create a database, if the file doesn't exist it creates the file
